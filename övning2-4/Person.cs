5a3ec5322c28f8cfbd57ebfaf3b25827e1a75b26
sere06001 <sere06001@utb.vaxjo.se>
2024-04-04 15:00:56 +0200

    Add project files.

---
+++ övning2-4/Person.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace övning2t4
+{
+    internal class Person
+    {
+        private string namn;
+        private string efternamn;
+        private DateTime födelsedag;
+        private bool myndig;
+        private int år;
+        private double vikt;
+        private double längd;
+        private double bmi;
+        public double Vikt
+        {
+            get { return vikt; }
+            set { vikt = value; }
+        }
+        public double Längd
+        {
+            get { return längd; }
+            set { längd = value; }
+        }
+        public double BMI
+        {
+            get { return bmi; }
+            set { bmi = value; }
+        }
+        public DateTime Födelsedag
+        {
+            get { return födelsedag; }
+            set
+            {
+                födelsedag = value;
+            }
+        }
+        public int År
+        {
+            get { return år; }
+            set
+            {
+                år = BeräknaÅlder(födelsedag);
+
+                if (år >= 18)
+                {
+                    Myndig = true;
+                }
+                else
+                {
+                    Myndig = false;
+                }
+            }
+        }
+
+        public bool Myndig
+        {
+            get { return myndig; }
+            set
+            { myndig = value; }
+        }
+        public int BeräknaÅlder(DateTime födelsedag)
+        {
+            DateTime nu = DateTime.Today;
+            int år = nu.Year - födelsedag.Year;
+
+            if (födelsedag.Date > nu.AddYears(-år))
+            {
+                år--;
+            }
+            return år;
+        }
+        public string Namn
+        {
+            get { return namn; }
+            set { namn = value; }
+        }
+        public string Efternamn
+        {
+            get { return efternamn; }
+            set { efternamn = value; }
+        }
+    }
+}
---
+++ övning2-4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+namespace övning2t4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Skriv förnamn: ");
+            string förnamn = Console.ReadLine();
+            Console.Write("Skriv efternamn: ");
+            string efternamn = Console.ReadLine();
+            Person person = new Person();
+            person.Namn = förnamn;
+            person.Efternamn = efternamn;
+            Console.WriteLine();
+
+            Console.Write($"Vilket år är du född?: ");
+            int år = int.Parse(Console.ReadLine());
+            Console.Write("Vilken månad är du född? Skriv månaden som siffra: ");
+            int månad = int.Parse(Console.ReadLine());
+            Console.Write("Vilken dag är du född? Skriv som siffra: ");
+            int dag = int.Parse(Console.ReadLine());
+            person.Födelsedag = new DateTime(år, månad, dag);
+            person.År = person.BeräknaÅlder(person.Födelsedag);
+            Console.WriteLine();
+
+            Console.Write("Hur lång är du i cm?: ");
+            person.Längd = double.Parse(Console.ReadLine());
+            Console.Write("Hur mycket väger du i kg?: ");
+            person.Vikt = double.Parse(Console.ReadLine());
+
+            person.BMI = Math.Round(person.Vikt / (person.Längd / 100 * person.Längd / 100), 1);
+
+            Console.WriteLine();
+            Console.Write($"Du heter {person.Namn} {person.Efternamn}, din BMI är {person.BMI}, du är {person.År} och du är ");
+            if (person.Myndig)
+            {
+                Console.Write("myndig.");
+            }
+            else
+            {
+                Console.Write("inte myndig.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
